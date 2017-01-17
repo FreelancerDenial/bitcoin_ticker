193052f0ccecfc08428fa224aac8e224f5981a28
Denial <thegosts@gmail.com>
2017-01-17 18:23:16 +0200

    First ver.

--- src/main/main.go
+++ src/main/main.go
@@ -1,11 +1,82 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 )
+
+var (
+	BtceAdrr      = "https://btc-e.com/api/3/ticker/eur_usd-btc_usd-btc_eur"
+	BtcChurtsAdrr = "http://api.bitcoincharts.com/v1/markets.json"
+)
+
+func BtceUpd() (btc_euro, btc_usd, eur_usd float64, ok bool) {
+	var BtceList *btce_ticker
+
+	client := &http.Client{}
+	responce, err := client.Get(BtceAdrr)
+	if err != nil {
+		log.Printf("Getting err on sending msg to API: %v", err)
+		return 0, 0, 0, false
+	}
+	defer responce.Body.Close()
+
+	err = json.NewDecoder(responce.Body).Decode(&BtceList)
+	if err != nil {
+		log.Printf("Getting err on reading HTML responce from API: %v", err)
+		return 0, 0, 0, false
+	}
+
+	return BtceList.BTCEuro.EuroCurrency, BtceList.BTCUsd.UsdCurrency, BtceList.EuroUsd.EuroUsdCurrency, true
+}
+
+func BtcChurtsUpd() (btc_euro, btc_usd, eur_usd float64, ok bool) {
+	var BtcChurtsList []*btc_charts
+
+	client := &http.Client{}
+	responce, err := client.Get(BtcChurtsAdrr)
+	if err != nil {
+		log.Printf("Getting err on sending msg to API: %v", err)
+		return 0, 0, 0, false
+	}
+	defer responce.Body.Close()
+
+	err = json.NewDecoder(responce.Body).Decode(&BtcChurtsList)
+	if err != nil {
+		log.Printf("Getting err on reading HTML responce from API: %v", err)
+		return 0, 0, 0, false
+	}
+
+	var UsdSum, EurSum float64
+	var UsdCount, EurCount float64
+	for _, currency := range BtcChurtsList {
+		switch currency.Currency {
+		case "USD":
+			if currency.AvgPrice != 0 {
+				UsdSum += currency.AvgPrice
+				UsdCount++
+			}
+		case "EUR":
+			if currency.AvgPrice != 0 {
+				EurSum += currency.AvgPrice
+				EurCount++
+			}
+		}
+	}
+
+	return EurSum / EurCount, UsdSum / UsdCount, 0, true
+
+}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	fmt.Println("Start working!")
+
+	a, b, c, flag := BtceUpd()
+	fmt.Println(a, b, c, flag)
+
+	a, b, c, flag = BtcChurtsUpd()
+	fmt.Println(a, b, c, flag)
 }
--- src/main/struct.go
+++ src/main/struct.go
@@ -1,1 +1,24 @@
 package main
+
+type btce_ticker struct {
+	BTCEuro *btce_euro     `json:"btc_eur"`
+	BTCUsd  *btce_usd      `json:"btc_usd"`
+	EuroUsd *btce_euro_usd `json:"eur_usd"`
+}
+
+type btce_euro struct {
+	EuroCurrency float64 `json:"avg"`
+}
+
+type btce_usd struct {
+	UsdCurrency float64 `json:"avg"`
+}
+
+type btce_euro_usd struct {
+	EuroUsdCurrency float64 `json:"avg"`
+}
+
+type btc_charts struct {
+	Currency string  `json:"currency"`
+	AvgPrice float64 `json:"avg"`
+}
